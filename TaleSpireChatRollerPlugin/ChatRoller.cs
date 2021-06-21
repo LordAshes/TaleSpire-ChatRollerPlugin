e08fbe767a50043cf3897a35d9dc626a7caafede
Lord Ashes <webtable@gmail.com>
2021-06-21 08:57:15 -0400

    Version  1.4.1: Exposed Roll Handler
    Exposed Roll Handler for parent plugins to make use for the roller

---
+++ TaleSpireChatRollerPlugin/ChatRoller.cs
@@ -0,0 +1,138 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Bounce.Unmanaged;
+using System.Linq;
+using UnityEngine;
+
+namespace LordAshes
+{
+    [BepInPlugin(Guid, "Chat Roller Plug-In", Version)]
+    [BepInDependency(RadialUI.RadialUIPlugin.Guid)]
+    public partial class ChatRollerPlugin : BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Guid = "org.lordashes.plugins.chatroller";
+        public const string Version = "1.4.1.0";
+
+        // Content directory
+        public static string dir = UnityEngine.Application.dataPath.Substring(0, UnityEngine.Application.dataPath.LastIndexOf("/")) + "/TaleSpire_CustomData/";
+
+        // Configuration
+        private ConfigEntry<KeyboardShortcut>[] triggerKey { get; set; } = new ConfigEntry<KeyboardShortcut>[2];
+
+        // Radial menu creature
+        private CreatureGuid radialCreature = CreatureGuid.Empty;
+
+        /// <summary>
+        /// This function is called once by TaleSpire
+        /// </summary>
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Chat Roller Plugin Active. Expecting character sheets in '" + dir + "/Misc',");
+            UnityEngine.Debug.Log("with file name matching the edition dot character name and an .CHS extension.");
+
+            // Read configuration
+            triggerKey[0] = Config.Bind("Keys", "Select Character Sheet Edition", new KeyboardShortcut(KeyCode.E, KeyCode.LeftControl));
+            triggerKey[1] = Config.Bind("Keys", "Make A Roll", new KeyboardShortcut(KeyCode.R, KeyCode.LeftControl));
+            RollHandler.SetEdition(Config.Bind("Settings", "Edition Prefix", "Dnd5e").Value);
+
+            Debug.Log("Chat Roller Plugin: Using Edition '" + RollHandler.GetEdition() + "'");
+
+            // Add icon to radial mini menu if config dictates it
+            if (Config.Bind("Settings", "Show In Mini Menu", true).Value)
+            {
+                // Add icon to radial menu
+                Texture2D tex = new Texture2D(32, 32);
+                tex.LoadImage(System.IO.File.ReadAllBytes(dir + "Images/Icons/Dice.Png"));
+                Sprite icon = Sprite.Create(tex, new Rect(0, 0, 32, 32), new Vector2(0.5f, 0.5f));
+
+                RadialUI.RadialUIPlugin.AddOnCharacter(Guid, new MapMenu.ItemArgs
+                {
+                    Action = (mmi, obj) =>
+                    {
+                        SystemMessage.AskForTextInput("Roller", "What Should I Roll: ", "Roll", (rollcode) => { RollHandler.ProcessRollRequest(radialCreature, rollcode); }, null, "Cancel", null);
+                    },
+                    Icon = icon,
+                    Title = "Roller",
+                    CloseMenuOnActivate = true
+                }, Reporter);
+            }
+
+            // Post plugin on the TaleSpire main page
+            StateDetection.Initialize(this.GetType());
+        }
+
+        /// <summary>
+        /// Method to track which asset has the radial menu open
+        /// </summary>
+        /// <param name="selected"></param>
+        /// <param name="radialMenu"></param>
+        /// <returns></returns>
+        private bool Reporter(NGuid selected, NGuid radialMenu)
+        {
+            radialCreature = new CreatureGuid(radialMenu);
+            return true;
+        }
+
+        /// <summary>
+        /// This function is called periodically by TaleSpire.
+        /// </summary>
+        void Update()
+        {
+            // Ensure that there is a camera controller instance
+            if (CameraController.HasInstance)
+            {
+                // Ensure that there is a board session manager instance
+                if (BoardSessionManager.HasInstance)
+                {
+                    // Ensure that there is a board
+                    if (BoardSessionManager.HasBoardAndIsInNominalState)
+                    {
+                        // Ensure that the board is not loading
+                        if (!BoardSessionManager.IsLoading)
+                        {
+                            // Check for roll shortcut
+                            if (StrictKeyCheck(triggerKey[1].Value))
+                            {
+                                SystemMessage.AskForTextInput("Roller", "What Should I Roll: ", "Roll", (rollcode) => { RollHandler.ProcessRollRequest(LocalClient.SelectedCreatureId, rollcode); }, null, "Cancel", null);
+                            }
+                            else if (StrictKeyCheck(triggerKey[0].Value))
+                            {
+                                SystemMessage.AskForTextInput("Roller", "What Is The Edition: ", "Set", (edition) => { RollHandler.SetEdition(edition); }, null, "Cancel", null);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static string GetEdition()
+        {
+            return RollHandler.GetEdition();
+        }
+
+        public static void SetEdition(string setting)
+        {
+            RollHandler.SetEdition(setting);
+        }
+
+        public static string GetCreatureName(CreatureBoardAsset asset)
+        {
+            string name = asset.Creature.Name;
+            if (name.Contains("<size=0>")) { name = name.Substring(0, name.IndexOf("<size=0>")).Trim(); }
+            return name;
+        }
+
+        public bool StrictKeyCheck(KeyboardShortcut check)
+        {
+            // Check main key
+            if (!check.IsUp()) { return false; }
+            // Check that specified modifiers are pressed while all other modifiers are not pressed
+            foreach (KeyCode modifier in new KeyCode[] { KeyCode.LeftAlt, KeyCode.RightAlt, KeyCode.LeftControl, KeyCode.RightControl, KeyCode.LeftShift, KeyCode.RightShift })
+            {
+                if (Input.GetKey(modifier) != check.Modifiers.Contains(modifier)) { return false; }
+            }
+            return true;
+        }
+    }
+}
---
+++ TaleSpireChatRollerPlugin/RollHandler.cs
@@ -0,0 +1,189 @@
+﻿using System.Data;
+
+namespace LordAshes
+{
+    public partial class ChatRollerPlugin
+    {
+        public static class RollHandler
+        {
+            // Randomizer
+            private static System.Random rnd = new System.Random();
+
+            // Edition
+            private static string edition;
+
+            /// <summary>
+            /// Method to process char requests
+            /// </summary>
+            /// <param name="request">Chat request without the identifier</param>
+            /// <returns>Message to be displayed or null</returns>
+            public static void ProcessRollRequest(CreatureGuid activeObjectId, string request)
+            {
+                UnityEngine.Debug.Log("Roll Request: " + request);
+
+                CreatureBoardAsset activeObject;
+                CreaturePresenter.TryGetAsset(activeObjectId, out activeObject);
+
+                string expanded = request;
+
+                UnityEngine.Debug.Log("Replacing Place Holders Based On Edition '"+GetEdition()+"'");
+
+                expanded = ReplacePlaceHolders(expanded, activeObject);
+
+                UnityEngine.Debug.Log("Modified Roll Request After Placeholder Replacement: " + expanded);
+
+                expanded = ReplaceDiceRolls(expanded);
+
+                UnityEngine.Debug.Log("Modified Roll Request After Dice Roll Replacement: " + expanded);
+
+                DataTable dt = new DataTable();
+
+                object v = null;
+                try
+                {
+                    v = dt.Compute(expanded.Replace("=", "~"), "");
+
+                    // string msg = "<align=\"center\"><size=24>" + WordCase(request) + "</size></align>\r\n<align=\"center\"><size=20>" + expanded + "</size></align>\r\n<align=\"center\"><size=32>" + v.ToString().Replace("~", "=") + "</size></align>";
+                    string msg = "<align=\"center\">" + WordCase(request) + "\r\n" + expanded + "\r\n" + v.ToString().Replace("~", "=") + "</align>";
+                    ChatManager.SendChatMessage(msg, activeObject.Creature.CreatureId.Value);
+                }
+                catch (System.Exception)
+                {
+                    SystemMessage.DisplayInfoText("Roll Error: Resolved down to:\r\n" + expanded);
+                }
+
+            }
+
+            /// <summary>
+            /// Allows setting an edition. This can be used by plugins that depend on this plugin.
+            /// </summary>
+            /// <param name="setting"></param>
+            public static void SetEdition(string setting)
+            {
+                if (setting != "") { edition = setting + "."; } else { edition = ""; }
+            }
+
+            /// <summary>
+            /// Method to get the edition
+            /// </summary>
+            /// <returns></returns>
+            public static string GetEdition()
+            {
+                return edition;
+            }
+
+            private static string ReplacePlaceHolders(string request, CreatureBoardAsset activeObject)
+            {
+                if (activeObject == null) { UnityEngine.Debug.LogWarning("No Mini Selected. Placeholder Replacement Cannot Be Performed."); return request; }
+
+                UnityEngine.Debug.Log("Using '" + ChatRollerPlugin.dir + "Misc/" + GetEdition() + ChatRollerPlugin.GetCreatureName(activeObject) + ".chs' for lookup data.");
+                string charSheet = ChatRollerPlugin.dir + "Misc/" + GetEdition() + ChatRollerPlugin.GetCreatureName(activeObject) + ".chs";
+                if (!System.IO.File.Exists(charSheet)) { UnityEngine.Debug.LogWarning("Missing Character Sheet '" + charSheet + "'."); return request; }
+
+                string[] replacements = System.IO.File.ReadAllLines(charSheet);
+
+                for (int l = 0; l < 5; l++)
+                {
+                    string[] segments = request.Split('\'');
+                    for (int s = 0; s < segments.Length; s = s + 2)
+                    {
+                        foreach (string rep in replacements)
+                        {
+                            string[] parts = rep.Split('=');
+                            if (parts.Length == 2)
+                            {
+                                segments[s] = segments[s].Replace(parts[0], parts[1]);
+                            }
+                        }
+                    }
+                    request = segments[0];
+                    for (int s = 1; s < segments.Length; s++)
+                    {
+                        request = request + "'" + segments[s];
+                    }
+                    UnityEngine.Debug.Log("Modified Request Is Now: " + request);
+                }
+
+                return request;
+            }
+
+            private static string ReplaceDiceRolls(string request)
+            {
+                bool quoted = false;
+                for (int i = 0; i < request.Length; i++)
+                {
+                    if (request.Substring(i, 1) == "'") { quoted = !quoted; }
+                    if (!quoted)
+                    {
+                        if (request.Substring(i, 1).ToUpper() == "D")
+                        {
+                            // Dice Notation
+                            int s = i - 1;
+                            while ("0123456789".Contains(request.Substring(s, 1)))
+                            {
+                                s = s - 1;
+                                if (s == -1) { break; }
+                            }
+                            s++;
+                            int numDice = int.Parse(request.Substring(s, i - s));
+                            int e = i + 1;
+                            while ("0123456789".Contains(request.Substring(e, 1)))
+                            {
+                                e = e + 1;
+                                if (e == request.Length) { break; }
+                            }
+                            e--;
+                            int numFaces = int.Parse(request.Substring(i + 1, e - i));
+                            string rolls = "(";
+                            for (int d = 0; d < numDice; d++)
+                            {
+                                int roll = rnd.Next(1, numFaces + 1);
+                                rolls = rolls + roll + "+";
+                            }
+                            rolls = rolls.Substring(0, rolls.Length - 1) + ")";
+                            request = request.Substring(0, s) + rolls + request.Substring(e + 1);
+                            i = 0;
+                        }
+                    }
+                }
+
+                UnityEngine.Debug.Log("Modified Request Is Now: " + request);
+
+                return request;
+            }
+
+            private static string WordCase(string txt)
+            {
+                string result = "";
+                int p = 0;
+                for (; p < txt.Length; p++)
+                {
+                    if (!("{_}".Contains(txt.Substring(p, 1))))
+                    {
+                        result = txt.Substring(p, 1).ToUpper();
+                        p++;
+                        break;
+                    }
+                }
+                for (; p < txt.Length; p++)
+                {
+                    if ("{_}".Contains(txt.Substring(p, 1)))
+                    {
+                        result = result + " ";
+                    }
+                    else if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".Contains(txt.Substring(p - 1, 1)))
+                    {
+                        // Previous character was a letter
+                        result = result + txt.Substring(p, 1).ToLower();
+                    }
+                    else
+                    {
+                        // Previous character was not a letter
+                        result = result + txt.Substring(p, 1).ToUpper();
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
